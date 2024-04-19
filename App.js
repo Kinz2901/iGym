9361048dba94edb2865d1d2c5e3c02b360153893
Guilherme Lins <guilhermelins978@gmail.com>
2024-04-19 16:38:45 -0300

    s

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Escolha seu nível de treino</Text>
       <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/fotinhaIniciante.png')} style={styles.blocoIniciante}>
+        <Text style={styles.textIniciante}>Iniciante</Text>
+      </ImageBackground>
+      <View style={styles.blocoIniciante}>
+        <Text style={styles.textIniciante}>Intermediário</Text>
+      </View>
+      <View style={styles.blocoIniciante}>
+        <Text style={styles.textIniciante}>Avançado</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#424242',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    color: '#FFFFFF',
+    fontSize: 20,
+  },
+  blocoIniciante: {
+  
+  },  
+  textIniciante: {
+    color: '#FFFFFF',
+  }
 });
