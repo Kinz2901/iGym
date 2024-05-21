23575e7cf1d6593f3634099ed31b6ace0c99aeaf
Guilherme Lins <guilhermelins978@gmail.com>
2024-05-21 17:55:39 -0300

    s

---
+++ src/app/Avançado/Treinos/quadriceps.jsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet } from "react-native";
+import listExercicios from "../../../../Listas de treinos avançado/pernaInt.json";
+import BlocoExercicio from "../../Exercício/blocoExercicios";
+
+export default function QuadricepsAv({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Quadríceps</Text>
+        <View style={styles.blocos}>
+          {listExercicios.map((exercicio) => (
+            <BlocoExercicio
+              key={exercicio.name}
+              name={exercicio.name}
+              image={exercicio.image}
+              series={exercicio.series}
+              descricao={exercicio.descricao}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#424242",
+    alignItems: "center",
+  },
+  content: {
+    width: "80%",
+  },
+  title: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  blocos: {
+    width: "100%",
+    gap: 20,
+    alignItems: "center"
+  }
+});
