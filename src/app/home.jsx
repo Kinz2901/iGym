a9d21ac3ad73cd378265034d9f1fa66159b79f41
Guilherme Lins <guilhermelins978@gmail.com>
2024-05-22 18:53:38 -0300

    S

--- src/app/home.jsx
+++ src/app/home.jsx
@@ -1,16 +1,22 @@
-import { View, Text, StatusBar, StyleSheet } from "react-native"
-import { Link } from '@react-navigation/native';
+import { View, Text, StatusBar, StyleSheet, Image } from "react-native";
+import { Link } from "@react-navigation/native";
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
+      <Image
+        source={require("../assets/haltere.png")}
+        resizeMode="cover"
+        style={styles.haltere}
+      ></Image>
       <Text style={styles.title}>iGym</Text>
+      <Text style={styles.desc}>Bem vindo ao seu app de treino.</Text>
       <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
-      <Link to='/nivel' style={styles.button}>
-        Começe já!
+      <Link to="/nivel" style={styles.button}>
+        COMEÇE JÁ!
       </Link>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -18,19 +24,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#424242",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  haltere: {
+    marginTop: 180,
   },
   title: {
     fontSize: 40,
     fontWeight: "bold",
     color: "#ffffff",
   },
+  desc: {
+    marginTop: 20,
+    color: "#ffffff",
+  },
   button: {
     backgroundColor: "#FFFFFF",
     margin: 10,
     padding: 10,
-    borderRadius: 5, 
+    borderRadius: 5,
     fontWeight: "bold",
     color: "#424242",
-  }
-})
+    marginTop: 20,
+  },
+});
